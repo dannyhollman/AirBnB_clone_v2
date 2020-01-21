c994cd7f11e606a368d84500ac24bcb62771b6c8
Danny Hollman <dannyhollman@gmail.com>
2020-01-21 18:07:56 +0000

    [INIT] Add all files

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,30 @@
+from flask import Flask, abort
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello():
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>')
+def c(text):
+    return "C {}".format(text.replace('_', ' '))
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    return "Python {}".format(text.replace('_', ' '))
+
+@app.route('/number/<n>')
+def number(n):
+    if n.isnumeric():
+        return "{} is a number".format(n)
+    else:
+        abort(404)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
