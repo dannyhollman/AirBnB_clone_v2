0223d28c9a27366f98851ecfd8c35e30423d18e5
Danny Hollman <dannyhollman@gmail.com>
2020-01-23 02:27:57 +0000

    [INIT] Add files for 10

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from flask import Flask, abort, render_template
+from models import storage
+""" starts a flask web application """
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/hbnb_filters')
+def nav_bar():
+    """ display states """
+    states = storage.all('State')
+    return render_template('10-hbnb_filters.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(error):
+    """ close after request """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
